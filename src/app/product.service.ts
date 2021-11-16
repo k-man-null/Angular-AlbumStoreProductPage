79bdacb4de14a4c8a91e22f628ee707a8d3b9bed
Bill Konchellah <williamoriginal2019@gmail.com>
2021-11-16 23:58:33 +0300

    ""

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Album } from './album';
 import { map } from 'rxjs/operators';
+
+import { Album } from './album';
+
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl: string = '../assets/album.json'
+  private _productsUrl: string = '../assets/products.json'
   constructor(private _http:Http) { }
 
   getAlbum(id:number):Observable<Album> {
     return this._http.get(this._albumUrl).pipe(map(response => <Album>response.json()))
     }
+
+  getProducts():Observable<Product[]> {
+    return this._http.get(this._productsUrl).pipe(map(response => <Product[]>response.json()))
+  }
 }
