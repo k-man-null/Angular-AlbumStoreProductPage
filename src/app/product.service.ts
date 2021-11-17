36c0a1fa763cd94a2107964ac31e5612b5fcf53e
Bill Konchellah <williamoriginal2019@gmail.com>
2021-11-17 13:09:14 +0300

    "revision"

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+// import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 import { Album } from './album';
 
@@ -15,10 +16,10 @@ export class ProductService {
   constructor(private _http:Http) { }
 
   getAlbum(id:number):Observable<Album> {
-    return this._http.get(this._albumUrl).pipe(map(response => <Album>response.json()))
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
     }
 
   getProducts():Observable<Product[]> {
-    return this._http.get(this._productsUrl).pipe(map(response => <Product[]>response.json()))
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json())
   }
 }
